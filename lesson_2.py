b41ca31f4b805d848d568515e1565fbff446617d
mseewamayai <159874295+zerromatata@users.noreply.github.com>
2024-02-23 09:52:45 +0300

    Delete lesson_2.py

--- lesson_2.py
+++
@@ -1,7 +0,0 @@
-print("my name is zerro")
-print("19 years old")
-print("i come from nairobi")
-print("1 love watching anime")
-print("------------------")
-print("*******************")
-print("##################")
