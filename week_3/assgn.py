9311938f5196d1ed2e21eb53f3dc18460932c6e3
mseewamayai <159874295+zerromatata@users.noreply.github.com>
2024-02-27 20:57:03 -0800

    Add files via upload

---
+++ week_3/assgn.py
@@ -0,0 +1,19 @@
+movies = ["Hells parasise", "Apex", "Silence","Ruby", "Counter Strike"] 
+print(movies)
+
+movies.reverse()
+print("\n")
+print(movies)
+
+movies.pop()
+print("\n")
+print(movies)
+
+movies.sort()
+print("\n")
+print(movies)
+
+
+movies.count()
+print("\n")
+print(movies)
--- week_3/lists.py
+++ week_3/lists.py
@@ -1,2 +1,33 @@
 items = ["pen","set","ruler","laptop"]
 print(items)
+items.append("phone")
+print("\n")
+print(items)
+
+# append,remove,insert,count,index,reverse
+
+items.pop()
+print("\n")
+print(items)
+
+items.sort()
+print("\n")
+print(items)
+
+
+items.count()
+print("\n")
+print(items)
+
+items.reverse()
+print("\n")
+print(items)
+
+
+
+
+
+movies = ["Hells parasise", "Apex", "Silence","Ruby", "Counter Strike"]
+print(movies)
+
+
