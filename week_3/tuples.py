1d66a03d83f8c1c7ef2f85ef929828d772d3f964
mseewamayai <159874295+zerromatata@users.noreply.github.com>
2024-02-28 21:12:26 -0800

    Add files via upload

---
+++ week_3/tuples.py
@@ -0,0 +1,18 @@
+# type of list that doesnt change during program execution
+
+cities = ("Nairobi","Kampala","Naivasha","Kisumu","Mombasa")
+
+
+print(cities)
+
+for city in cities:
+    print(city)
+
+
+hobbies = ("sleeping","Gaming","eating","reading","writing")
+print(hobbies)
+
+for hobby in hobbies:
+    print("\n")
+    print(hobby)
+
